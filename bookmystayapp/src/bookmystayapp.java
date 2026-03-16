fae30fe7a7679f6c7890b21bb05f91f2a519e7cb
RAZEEN18 <razeenmujeeb143@gmail.com>
2026-03-16 14:43:07 +0530

    uc1hostelbookingapp

--- bookmystayapp/src/bookmystayapp.java
+++ bookmystayapp/src/bookmystayapp.java
@@ -5,7 +5,7 @@ public class bookmystayapp {
         System.out.println("Welcome to the Hotel Booking Management System");
         System.out.println("Application: Book My Stay App");
         System.out.println("Version: 1.0");
-        System.out.println("System initialized successfully.");
+        System.out.println("System initialized successfully.    ");
 
     }
 }
