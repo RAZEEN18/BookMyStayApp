849dcf1148a646680fb9080410c22298061195c9
RAZEEN18 <razeenmujeeb143@gmail.com>
2026-03-16 15:59:13 +0530

    uc5

--- bookmystayapp/src/bookmystayapp.java
+++ bookmystayapp/src/bookmystayapp.java
@@ -1,11 +1,58 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Reservation {
+    String guestName;
+    String roomType;
+
+    public Reservation(String guestName, String roomType) {
+        this.guestName = guestName;
+        this.roomType = roomType;
+    }
+
+    public void displayReservation() {
+        System.out.println("Guest: " + guestName + " | Requested Room: " + roomType);
+    }
+}
+
+class BookingRequestQueue {
+
+    private Queue<Reservation> requestQueue;
+
+    public BookingRequestQueue() {
+        requestQueue = new LinkedList<>();
+    }
+
+    public void addRequest(Reservation reservation) {
+        requestQueue.offer(reservation);
+        System.out.println("Booking request added for " + reservation.guestName);
+    }
+
+    public void displayQueue() {
+        System.out.println("\n===== Current Booking Request Queue =====");
+
+        for (Reservation r : requestQueue) {
+            r.displayReservation();
+        }
+
+        System.out.println("-----------------------------------------");
+    }
+}
+
 public class bookmystayapp {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to the Hotel Booking Management System");
-        System.out.println("Application: Book My Stay App");
-        System.out.println("Version: 1.0");
-        System.out.println("System initialized successfully.    ");
+        BookingRequestQueue queue = new BookingRequestQueue();
 
+        Reservation r1 = new Reservation("Arjun", "Single Room");
+        Reservation r2 = new Reservation("Priya", "Double Room");
+        Reservation r3 = new Reservation("Rahul", "Suite Room");
+
+        queue.addRequest(r1);
+        queue.addRequest(r2);
+        queue.addRequest(r3);
+
+        queue.displayQueue();
     }
 }
